7756d603e1c22a97ab287ab039a6e6cf08dc23f3
RICHARD44720 <richard27.2005@gmail.com>
2025-12-09 09:01:16 +0530

    Time: 9 ms (15.38%), Space: 44.4 MB (12.04%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        BigInteger a =new BigInteger(num1);
+        BigInteger b =new BigInteger(num2);
+
+        BigInteger product = a.multiply(b);
+
+        return String.valueOf(product);
+    }
+}
