cdbf8b3c4331e873ffc419be21845581b6b80d54
RICHARD44720 <richard27.2005@gmail.com>
2026-01-20 18:15:37 +0530

    Time: 1 ms (22.05%), Space: 48.2 MB (80.38%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int l=0;int r=s.length-1;
+        while(r>l){
+            char temp=s[r];
+            s[r]=s[l];
+            s[l]=temp;l++;r--;
+        }
+        
+    }
+}
