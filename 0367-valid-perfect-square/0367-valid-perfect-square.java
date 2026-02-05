a3bd044d8c9753911a6347be9de560f4d96da9ce
RICHARD44720 <richard27.2005@gmail.com>
2026-02-05 09:41:57 +0530

    Time: 0 ms (100%), Space: 42.4 MB (15%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start = 1, end = num/2 + 1, mid = start + (end-start)/2;
+        while( start <= end){
+            mid = start + (end-start)/2;
+            if(mid*mid == num){
+                return true;
+            }
+            else if(mid*mid > num){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+}
